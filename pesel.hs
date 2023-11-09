c46495f3efce23ae67c036bb4324293d11a1bdf6
m4dox21 <132065931+m4dox21@users.noreply.github.com>
2023-11-09 01:12:07 +0100

    Update pesel.hs

--- pesel.hs
+++ pesel.hs
@@ -3,18 +3,23 @@ import Data.Char(digitToInt)
 
 plec' :: Char -> Char
 plec' x 
-    |   x == '0' = 'k'
-    |   x == '2' = 'k'
-    |   x == '4' = 'k'
-    |   x == '6' = 'k'
-    |   x == '8' = 'k'
-    |   otherwise = 'm'
+    | digitToInt(x) `mod` 2 == 0    = 'k'
+    | otherwise = 'm'
 
 plec :: [Char] -> Char
 plec (x) = plec' (x !! 9)
 
+rok' :: Int -> Int
+rok' x
+    | x >= 81 && x <= 92    = 1800
+    | x >= 01 && x <= 12    = 1900  
+    | x >= 21 && x <= 32    = 2000  
+    | x >= 41 && x <= 52    = 2100  
+    | x >= 61 && x <= 72    = 2200  
+    | otherwise = error "bledny rok"
+
 rok :: [Char] -> Int
-rok (x) = (digitToInt(x !! 0))*10 + (digitToInt (x !! 1))
+rok (x) = (rok' ((digitToInt(x !! 2))*10 + digitToInt(x !! 3))) + ((digitToInt(x !! 0))*10 + (digitToInt(x !! 1)))
 
 miesiac' :: Int -> Int
 miesiac' x
@@ -23,6 +28,7 @@ miesiac' x
     | x >= 41 && x <= 52   =   x - 40
     | x >= 61 && x <= 72   =   x - 60
     | otherwise = x
+
 
 miesiac :: [Char] -> Int
 miesiac (x) = miesiac' ((digitToInt(x !! 2))*10 + (digitToInt (x !! 3)))
@@ -46,17 +52,39 @@ poprawnaCyfraKontrolna x
     | obliczonaCyfraKontrolna(x) == cyfraKontrolna(x)   = True
     | otherwise                                         = False
 
--- poprawnaData :: [Char] -> Bool
+leapYear' :: Int -> Int
+leapYear' x =
+    if x `mod` 400 == 0 then
+        1
+    else
+        if x `mod` 100 == 0 then
+            0
+        else
+            if x `mod` 4 == 0 then
+                1
+            else
+                0
 
-ileDni :: [Char] -> Int 
+leapYear :: [Char] -> Int
+leapYear (x) = leapYear'(rok(x))
+
+ileDni :: [Char] -> Int
 ileDni x
-    | miesiac(x) == 2                                                               = 28
+    | miesiac(x) == 2 && leapYear(x) == 1                                           = 29
+    | miesiac(x) == 2 && leapYear(x) == 0                                           = 28
     | miesiac(x) == 4 || miesiac(x) == 6 || miesiac(x) == 9 || miesiac(x) == 11     = 30
     | otherwise                                                                     = 31
 
+poprawnaData :: [Char] -> Bool
+poprawnaData x
+    | (dzien(x) > 0 && dzien(x) <= ileDni(x) ) && (miesiac(x) >= 1 && miesiac(x) <= 12) && (rok(x) >= 1800 && rok(x) <= 2299)   = True
+    | otherwise = False
 
+poprawnyPesel :: [Char] -> Bool
+poprawnyPesel x
+    | length(x) == 11 && poprawnaData(x) == True && poprawnaCyfraKontrolna(x) == True      = True
+    | otherwise = False
 
--- poprawnyPesel :: [Char] -> Bool
 -- poprawnePesele :: [[Char]] -> Bool
 -- liczbyPlci :: [[Char]] -> (Int,Int)
 -- najmlodszy :: [[Char]] -> (Int,Int,Int)
